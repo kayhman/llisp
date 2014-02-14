ea28b50274a2cb66b3e40daf1e859cb5918fc382
guillaume.saupin@gmail.com <guillaume.saupin@gmail.com>
2014-02-14 22:54:55 +0100

    Parsing ok !

--- parse.cpp
+++ parse.cpp
@@ -5,26 +5,54 @@
 
 struct Cell
 {
+  virtual ~Cell() {};
 };
 
 struct Sexp : public Cell
 {
   std::vector<Cell*> cells;
+
+  void print(const std::string& code);
 };
 
 struct Atom : public Cell
 {
   int begin, end;
+  void print(const std::string& code);
 };
+
+void Atom::print(const std::string& code)
+{
+  for(int c = this->begin ; c < this->end ; ++c)
+    std::cout << code[c];
+  std::cout << " ";
+}
+
+void Sexp::print(const std::string& code)
+{
+  for(int cId = 0 ; cId < cells.size() ; ++cId)
+    {
+      Cell* cl = this->cells[cId];
+      Atom* at = dynamic_cast<Atom*>(cl);
+      Sexp* sx = dynamic_cast<Sexp*>(cl);
+      
+      std::cout << "[";
+      if(at)
+	at->print(code);
+      if(sx)
+	sx->print(code);
+      std::cout << "]";
+    }
+}
 
 void parse(const std::string& code)
 {
   bool newToken = false;
   Atom curTok;
-  std::vector<Atom> tokens;
   curTok.begin = -1;
 
-  Sexp sexp;
+  std::vector<Sexp*> sexps;
+  Sexp* sexp = NULL;
 
   for(int c = 0 ; c < code.size() ; ++c)
     {
@@ -33,44 +61,51 @@ void parse(const std::string& code)
 	 code[c] == ' ' ||
 	 code[c] == ')')
 	{
+	  if(code[c] == '(')
+	    {
+	      Sexp* sx = new Sexp();
+	      if(sexp)
+		sexp->cells.push_back(sx);
+	      sexp = sx;
+	      sexps.push_back(sx);
+	    }
+
 	  if(curTok.begin == -1)
 	    newToken = true;
 	  else
 	    {
 	      newToken = true;
 	      curTok.end = c;
-	      tokens.push_back(curTok);
 	      
 	      Atom* at = new Atom();
 	      *at = curTok;
 	      
-	      sexp.cells.push_back(at);
+	      sexp->cells.push_back(at);
 
 	      curTok.begin =  -1;
 	    }
+
+	  if(code[c] == ')')
+	    {
+	      sexp = sexps.back();
+	      sexps.pop_back();
+	    }
+
 	}
       else if(isalnum(code[c]) && newToken)
 	{
 	  newToken = false;
 	  curTok.begin = c;
 	}
-	
     }
-  std::cout << std::endl;
-  
-  std::cout << "#tokens " << tokens.size() << std::endl;
+  std::cout << "#tokens " << sexp->cells.size() << std::endl;
 
-  for(int tId = 0 ; tId < tokens.size() ; ++tId)
-    {
-      for(int c = tokens[tId].begin ; c < tokens[tId].end ; ++c)
-	std::cout << code[c];
-      std::cout << std::endl;
-    }
+  sexp->print(code);
 
 }
 
 int main(int argc, char* argv[])
 {
-  parse("(lapin 2 3)");
+  parse("(lapin 2 (add 3 2))");
   return 0;
 }
