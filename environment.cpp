351c513634f0b3f90c2ecfd7a0e5ccd012fa8f84
guillaume.saupin@gmail.com <guillaume.saupin@gmail.com>
2014-03-10 22:09:01 +0100

    Added missing file.

---
+++ environment.cpp
@@ -0,0 +1,52 @@
+#include "environment.h"
+#include "cell.h"
+
+template <typename Key, typename Val>
+Env<Key, Val>::Env()
+{
+  envs.push_back(&top);
+}
+
+template <typename Key, typename Val>
+void Env<Key, Val>::addEnvMap(std::map<Key, Val>* env)
+{
+  this->envs.push_back(env);
+}
+
+template <typename Key, typename Val>
+void Env<Key, Val>::removeEnv()
+{
+  this->envs.pop_back();
+}
+
+template <typename Key, typename Val>
+Val& Env<Key, Val>::operator[] (const Key& k)
+{
+  for(auto envIt = envs.rbegin() ; envIt != envs.rend() ; envIt++)
+    if((*envIt)->find(k) != (*envIt)->end())
+ 	return (**envIt)[k];
+   return (*envs.back())[k];
+}
+
+template <typename Key, typename Val>
+Val& Env<Key, Val>::operator[] (Key& k)
+{
+  for(auto envIt = envs.rbegin() ; envIt != envs.rend() ; envIt++)
+    if((*envIt)->find(k) != (*envIt)->end())
+ 	return (**envIt)[k];
+   return (*envs.back())[k];
+}
+
+template <typename Key, typename Val>
+bool Env<Key, Val>::find (const Key& k)
+{
+  for(auto envIt = envs.rbegin() ; envIt != envs.rend() ; envIt++)
+    if((*envIt)->find(k) != (*envIt)->end())
+	return true;
+  return false;
+}
+
+template<typename K, typename V> std::map<K, V> Env<K,V>::top;
+template<typename K, typename V> std::map<K, std::function<V(Sexp* sexp, Env<K,V>& env)> > Env<K,V>::evalHandlers;
+
+template class  Env<std::string,std::shared_ptr<Cell> >;
