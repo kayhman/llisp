2664352ce73e270065f50c12273e94ce5f99eb02
guillaume <gsaupin@planisware.com>
2014-04-25 16:46:39 +0200

    Work in progress.

--- llvm/build.sh
+++ llvm/build.sh
@@ -3,7 +3,7 @@ g++ `llvm-config --ldflags` -o hw hw.o `llvm-config --libs engine scalaropts` -p
 
 
 clear
-clang++ -std=c++11 `llvm-config --cxxflags` -o run-bc.o -c run-bc.cpp
-clang++ -std=c++11 `llvm-config --ldflags` -o run-bc run-bc.o `llvm-config --libs engine scalaropts` -pthread -ldl
+#clang++ -std=c++11 `llvm-config --cxxflags` -o run-bc.o -c run-bc.cpp
+#clang++ -std=c++11 `llvm-config --ldflags` -o run-bc run-bc.o `llvm-config --libs engine scalaropts` -pthread -ldl
 
 
--- llvm/hw.cpp
+++ llvm/hw.cpp
@@ -7,6 +7,60 @@
 #include <vector>
 #include <string>
 
+
+static Function *CreateFibFunction(Module *M, LLVMContext &Context) {
+  // Create the fib function and insert it into module M. This function is said
+  // to return an int and take an int parameter.
+  Function *FibF =
+    cast<Function>(M->getOrInsertFunction("fib", Type::getInt32Ty(Context),
+                                          Type::getInt32Ty(Context),
+                                          (Type *)0));
+
+  // Add a basic block to the function.
+  BasicBlock *BB = BasicBlock::Create(Context, "EntryBlock", FibF);
+
+  // Get pointers to the constants.
+  Value *One = ConstantInt::get(Type::getInt32Ty(Context), 1);
+  Value *Two = ConstantInt::get(Type::getInt32Ty(Context), 2);
+
+  // Get pointer to the integer argument of the add1 function...
+  Argument *ArgX = FibF->arg_begin();   // Get the arg.
+  ArgX->setName("AnArg");            // Give it a nice symbolic name for fun.
+
+  // Create the true_block.
+  BasicBlock *RetBB = BasicBlock::Create(Context, "return", FibF);
+  // Create an exit block.
+  BasicBlock* RecurseBB = BasicBlock::Create(Context, "recurse", FibF);
+
+  // Create the "if (arg <= 2) goto exitbb"
+  Value *CondInst = new ICmpInst(*BB, ICmpInst::ICMP_SLE, ArgX, Two, "cond");
+  BranchInst::Create(RetBB, RecurseBB, CondInst, BB);
+
+  // Create: ret int 1
+  ReturnInst::Create(Context, One, RetBB);
+
+  // create fib(x-1)
+  Value *Sub = BinaryOperator::CreateSub(ArgX, One, "arg", RecurseBB);
+  CallInst *CallFibX1 = CallInst::Create(FibF, Sub, "fibx1", RecurseBB);
+  CallFibX1->setTailCall();
+
+  // create fib(x-2)
+  Sub = BinaryOperator::CreateSub(ArgX, Two, "arg", RecurseBB);
+  CallInst *CallFibX2 = CallInst::Create(FibF, Sub, "fibx2", RecurseBB);
+  CallFibX2->setTailCall();
+
+
+  // fib(x-1)+fib(x-2)
+  Value *Sum = BinaryOperator::CreateAdd(CallFibX1, CallFibX2,
+                                         "addresult", RecurseBB);
+
+  // Create the return instruction and add it to the basic block
+  ReturnInst::Create(Context, Sum, RecurseBB);
+
+  return FibF;
+}
+
+
 int main()
 {
   llvm::LLVMContext & context = llvm::getGlobalContext();
@@ -14,11 +68,14 @@ int main()
   llvm::IRBuilder<> builder(context);
 
   llvm::FunctionType *funcType = llvm::FunctionType::get(builder.getVoidTy(), false);
+  llvm::Function *FibF = CreateFibFunction(module, context);
+
+
   llvm::Function *mainFunc = 
     llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
   llvm::BasicBlock *entry = llvm::BasicBlock::Create(context, "entrypoint", mainFunc);
   builder.SetInsertPoint(entry);
-
+  
   llvm::Value *helloWorld = builder.CreateGlobalStringPtr("hello world!\n");
 
   std::vector<llvm::Type *> putsArgs;
@@ -28,8 +85,12 @@ int main()
   llvm::FunctionType *putsType = 
     llvm::FunctionType::get(builder.getInt32Ty(), argsRef, false);
   llvm::Constant *putsFunc = module->getOrInsertFunction("puts", putsType);
-
+  
+  std::vector<GenericValue> Args(1);
+  Args[0].IntVal = APInt(32, n);
+  
   builder.CreateCall(putsFunc, helloWorld);
+  builder.CreateCall(fibF, Args[0]);
   builder.CreateRetVoid();
   module->dump();
 }
